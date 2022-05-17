497567f542b382aa0ebff9cb7b641b325f2c542e
Mingyou Sung <mingyousung@gmail.com>
2022-05-17 07:57:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main(args):
     output_dim = len(set(target))
 
     if args.gnn.lower() == "depgcn":
-        model = Dependency_GCN(input_dim=input_dim, hidden_dim=hidden_dim, output_dim=output_dim, dependency_list=dependency_list, num_layers=args.num_layers, dropout_rate=args.dropout_rate, reverse_case=True)
+        model = Dependency_GCN(input_dim=input_dim, hidden_dim=hidden_dim, output_dim=output_dim, dependency_list=dependency_list, num_layers=args.num_layers, dropout_rate=args.dropout_rate, reverse_case=args.reverse)
     elif args.gnn.lower() == "depgat":
         model = Dependency_GAT(input_dim=input_dim, hidden_dim=hidden_dim, output_dim=output_dim, alpha=args.alpha, dependency_list=dependency_list, num_layers=args.num_layers, dropout_rate=args.dropout_rate)
 
@@ -67,11 +67,12 @@ if __name__ == "__main__":
     parser.add_argument("--nlp_pipeline", default="spacy", type=str, help="NLP preprocessing pipeline.")
     parser.add_argument("--unk_ignore", default=True, help="Ignore unknown tokens.")
     parser.add_argument("--num_layers", default=1, type=int, help="The number of lstm/bilstm layers.")
-    parser.add_argument("--gnn", default="depgat", type=str, help="Type of dependency gnn layer. (depgcn, depgat)")
+    parser.add_argument("--gnn", default="depgcn", type=str, help="Type of dependency gnn layer. (depgcn, depgat)")
     parser.add_argument("--alpha", default=0.01, type=float, help="Negative slope that controls the angle of the negative slope of LeakyReLU")
     parser.add_argument("--epochs", default=100, type=int, help="The number of epochs for training.")
     parser.add_argument("--learning_rate", default=1e-2, type=float, help="Learning rate.")
     parser.add_argument("--dropout_rate", default=0.1, type=float, help="Dropout rate.")
+    parser.add_argument("--reverse", default=True, help="Applying reverse dependency cases (gov -> dep) or not.")
     args = parser.parse_args()
 
     main(args)
